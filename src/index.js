ecac8c5283dfcdc9882854d1166eec724e2146ac
Perova <perovaa.24@gmail.com>
2020-10-12 13:58:37 +0300

    init: start task reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+    let a = 0;
+    let b = 0;
+    if (n < 0) n *= -1;
+    while (n > 0) {
+      a = n % 10;
+      b = b *10 + a;
+      n = Math.floor(n / 10);
+    }
+  return b;
 }
